e125e8775949e9fb93eb8cc211da4e6707a66bdb
DriedSponge <jordan@driedsponge.net>
2023-01-19 20:11:09 -0800

    Installed TailwindCSS

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,11 @@
 import adapter from "@sveltejs/adapter-static";
-
+import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({ pages: "build", assets: "build" })
-	}
+	},
+	preprocess: vitePreprocess()
 };
 
 export default config;
